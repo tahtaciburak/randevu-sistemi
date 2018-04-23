09ef0b79f9d9cb5896aa086732d84fbfad77020a
tahtaciburak <tahtaciburak@gmail.com>
2018-04-24 00:23:03 +0300

    list appointments work properly

--- app/appointments.js
+++ app/appointments.js
@@ -81,110 +81,31 @@ router.get('/all/:user_id',isLoggedIn,function (req,res) {
 	})
 })
 
-
-
 router.post("/new",isLoggedIn,function (req,res) {
     var host_id = req.user.id
     var appointment_header = req.body.appointment_header
     var appointment_description = req.body.appointment_description
     var length = req.body.length //dakika cinsinden
-    var start_date = req.body.start_date
+	var start_date = req.body.start_date
+	var start_time = req.body.start_time
+
     var period = req.body.period
 
     if(period=="single"){
-
+		db.query("INSERT INTO Appointments (HostID,AppointmentHeader,AppointmentDescription,Length) VALUES(?,?,?,?)",[host_id,appointment_header,appointment_description,length],function (err,result) {
+			if (err) {
+				res.json({code:400,message:"DB_ERROR"})
+			}else{
+				res.redirect("/")
+			}
+		})
     }else if(period=="weekly"){
 
     }else if(period=="monthly"){
 
     }
-
-	db.query("INSERT INTO Appointments (owner_id,name,description,created_at,image_url) VALUES(?,?,?,NOW(),?)",[owner_id,channel_name,channel_description,image_url],function (err,result) {
-		if (err) {
-			res.json({code:400,message:"DB_ERROR"})
-		}else{
-			res.redirect("/profile")
-		}
-	})
-        utilities.createChannel(
-            {
-                channel_name: req.body.channel_name,
-                channel_description: req.body.channel_description,
-                image_url: req.body.img_url,
-                owner_id: req.user.id
-            },
-            function(err, id)
-            {
-                if(err)
-                {
-                    return utilities.printError(res, err)
-                }
-
-                //utilities.printSuccess(res, {channel_id: id})
-				res.redirect("/channels/" + id)
-            }
-        )
-})
-/*
-router.get("/delete",isLoggedIn,function (req,res) {
-	var id = req.query.id
-
-	db.query("DELETE FROM data WHERE channel_id = ? ",[id],function (err,result) {
-		if (err) {
-			res.json({code:400,message:"DB_ERROR_here"})
-		}else{
-			db.query("DELETE FROM api_keys WHERE channel_id=?",[id],function (err,result) {
-				if(err){
-					res.json(err)
-				}else{
-					db.query("DELETE FROM components WHERE channel_id=?",[id],function (err,result) {
-						if (err) {
-							res.json(err)
-						}else{
-							db.query("DELETE FROM channels WHERE id=?",[id],function (err,result) {
-								if(err){
-									res.json({code:400,message:"DB_ERROR"})
-								}else{
-									res.redirect("/profile")
-								}
-							})
-						}
-					})
-				}
-			})
-		}
-	})
-
 })
 
-
-router.get("/components/delete/:component_id",isLoggedIn,function (req,res) {
-	var component_id = req.params.component_id
-	var channel_id
-	db.query("SELECT * FROM components WHERE id=?",[component_id],function (err,result) {
-		if(err){
-			res.json(err)
-		}else{
-			channel_id=result[0].channel_id
-		}
-	})
-	db.query("DELETE FROM data WHERE component_id=?",[component_id],function (err,result) {
-		if(err){
-			res.json(err)
-		}else{
-			db.query("DELETE FROM api_keys WHERE component_id=?",[component_id],function (err,result) {
-				if(err){
-					res.json(err)
-				}else{
-					db.query("DELETE FROM components WHERE id=?",[component_id],function (err,result) {
-						res.redirect("/channels/"+channel_id+"#home")
-					})
-				}
-			})
-		}
-	})
-})
-*/
 function isLoggedIn(req, res, next) {
 
 	// if user is authenticated in the session, carry on
